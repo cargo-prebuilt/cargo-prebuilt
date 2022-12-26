e289fa4833184d2fc10274381ed2e154ed6b614d
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2022-12-25 21:18:04 -0500

    Updated freebsd script

--- scripts/install-freebsd.sh
+++ scripts/install-freebsd.sh
@@ -1,3 +1,28 @@
 #!/usr/bin/env bash
 
-FREEBSD_MIRROR=\"$(fetch_best_freebsd_mirror)\" install_freebsd_package memstat
+set -x
+set -euo pipefail
+
+# shellcheck disable=SC1091
+. lib.sh
+# shellcheck disable=SC1091
+. freebsd-common.sh
+# shellcheck disable=SC1091
+. freebsd-install.sh
+
+main() {
+    apt-get update && apt-get install --assume-yes --no-install-recommends \
+        curl \
+        dnsutils \
+        jq \
+        xz-utils
+
+    local url=
+    url=$(fetch_best_freebsd_mirror)
+    FREEBSD_MIRROR="${url}" setup_freebsd_packagesite
+    FREEBSD_MIRROR="${url}" install_freebsd_package memstat
+
+    rm "${0}"
+}
+
+main "${@}"
