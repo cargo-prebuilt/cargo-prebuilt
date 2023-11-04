cf838077dc83eea1280a562035a8ab8c73aaa16d
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2023-11-04 00:45:55 -0400

    Change how github public handles url

--- src/interact/github_public.rs
+++ src/interact/github_public.rs
@@ -3,36 +3,34 @@ use ureq::{Agent, Error};
 
 pub struct GithubPublic {
     agent: Agent,
-    slug: String,
+    pre_url: String,
 }
 impl GithubPublic {
     pub fn new(agent: Agent, slug: &str) -> Self {
+        let pre_url = format!("https://{}/releases/download", slug);
         Self {
             agent,
-            slug: slug.to_string(),
+            pre_url,
         }
     }
 
-    fn pre_url(&self, id: &str, version: &str, file: &str) -> String {
-        format!(
-            "https://{}/releases/download/{id}-{version}/{file}",
-            self.slug
-        )
+    fn url(&self, id: &str, version: &str, file: &str) -> String {
+        format!("{}/{id}-{version}/{file}", self.pre_url)
     }
 }
 impl Interact for GithubPublic {
     fn get_latest(&self, id: &str) -> Result<String, InteractError> {
-        let url = format!("https://{}/releases/download/stable-index/{id}", self.slug);
+        let url = format!("{}/stable-index/{id}", self.pre_url);
         call(&self.agent, &url)
     }
 
     fn get_str(&self, id: &str, version: &str, file_name: &str) -> Result<String, InteractError> {
-        let url = self.pre_url(id, version, file_name);
+        let url = self.url(id, version, file_name);
         call(&self.agent, &url)
     }
 
     fn get_blob(&self, id: &str, version: &str, file_name: &str) -> Result<Vec<u8>, InteractError> {
-        let url = self.pre_url(id, version, file_name);
+        let url = self.url(id, version, file_name);
         let mut bytes = Vec::new();
         match self.agent.get(&url).call() {
             Ok(response) => {
