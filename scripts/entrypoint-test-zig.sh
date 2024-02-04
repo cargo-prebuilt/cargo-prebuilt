1585c445a5658abe7c7ae4b750930657693c2fae
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2024-02-04 01:51:01 -0500

    Fix tar

--- scripts/entrypoint-test-zig.sh
+++ scripts/entrypoint-test-zig.sh
@@ -6,7 +6,7 @@ if ! zig version &> /dev/null; then
     echo "Installing zig..."
     pushd /tmp
     curl -fsSL https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz -o zig.tar.xz
-    tar -xzJf zig.tar.xz
+    tar -xvJf zig.tar.xz
     cp -r zig-linux-x86_64-0.11.0/* /usr/local/bin
     popd
 fi
