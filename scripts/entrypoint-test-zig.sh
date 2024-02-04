94cec3be68f465903ad125a644e720b352bad60a
harmless-tech <28830529+harmless-tech@users.noreply.github.com>
2024-02-04 01:41:50 -0500

    zig test

---
+++ scripts/entrypoint-test-zig.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if ! zig version &> /dev/null; then
+    echo "Installing zig..."
+    pushd /tmp
+    curl -fsSL https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz -o zig.tar.xz
+    tar -xzJf zig.tar.xz
+    cp -r zig-linux-x86_64-0.11.0/* /usr/local/bin
+    popd
+fi
+
+if ! cargo-zigbuild --version &> /dev/null; then
+    echo "Installing cargo-zigbuild..."
+    cargo-prebuilt --ci cargo-zigbuild
+fi
+
+exec cargo +$RUST_VERSION "$@"
